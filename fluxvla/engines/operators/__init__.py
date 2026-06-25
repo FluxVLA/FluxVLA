b25a4ae775e5fcfad11606439f18f7750578a876
hhuang-xu <1536066346@qq.com>
2026-06-25 20:29:42 +0800

    feat(operators): support UR base operator

--- fluxvla/engines/operators/__init__.py
+++ fluxvla/engines/operators/__init__.py
@@ -17,5 +17,6 @@ from .base_operator import BaseOperator  # noqa: F401, F403
 from .fluxbisim_aloha_operator import AlohaOperatorSim  # noqa: F401, F403
 from .franka_dual_operator import FrankaDualOperator  # noqa: F401, F403
 from .franka_operator import FrankaOperator  # noqa: F401, F403
+from .new_ur_operator import NewUROperator  # noqa: F401, F403
 from .tron2_operator import Tron2Operator  # noqa: F401, F403
 from .ur_operator import UROperator  # noqa: F401, F403
