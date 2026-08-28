78f10acc1755891c028be335237c9b11bc6d27b5
jikun <jikun@limxdynamics.com>
2026-08-28 17:27:16 +0800

    feat: add aligned RoboDojo training configs

--- fluxvla/datasets/__init__.py
+++ fluxvla/datasets/__init__.py
@@ -16,6 +16,7 @@ from .arm_dataset import *  # noqa: F401, F403
 from .balanced_dataset_wrapper import *  # noqa: F401, F403
 from .dataset_wrapper import *  # noqa: F401, F403
 from .parquet_dataset import *  # noqa: F401, F403
+from .robodojo_dataset import *  # noqa: F401, F403
 from .parquet_dataset_v3 import *  # noqa: F401, F403
 from .sarm_dataset import *  # noqa: F401, F403
 from .utils import *  # noqa: F401, F403
